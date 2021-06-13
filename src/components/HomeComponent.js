63a33422d99484ef3dd64cfad08ef1afe628ec04
runzen <55329832+run-zen@users.noreply.github.com>
2021-06-13 10:39:11 +0530

    animations added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
 import MyCarousel from "./MyCarousel";
 import CorporateLeaders from "./CorporateLeaders";
 import { HomeData } from "../shared/HomeData";
@@ -6,19 +8,21 @@ import { Link } from "react-router-dom";
 
 function Home() {
     const Models = () => {
-        return HomeData.images.map((item) => {
+        return HomeData.images.map((item, index) => {
             return (
-                <div key={item.title} className="model">
-                    <div className="img-thumbnail shadow-lg">
-                        <Link to={`${item.link}`} target="_blank">
-                            <img src={item.img} alt={item.title} />
-                            <div className="model-title">{item.title}</div>
-                        </Link>
-                        {/* <Link to={`/${item.title}`} className="model-details">
+                <Fade left duration={1000 + (index % 3) * 150}>
+                    <div key={item.title} className="model">
+                        <div className="img-thumbnail shadow-lg">
+                            <Link to={`${item.link}`} target="_blank">
+                                <img src={item.img} alt={item.title} />
+                                <div className="model-title">{item.title}</div>
+                            </Link>
+                            {/* <Link to={`/${item.title}`} className="model-details">
                             <h5>click for more ...</h5>
                         </Link> */}
+                        </div>
                     </div>
-                </div>
+                </Fade>
             );
         });
     };
@@ -27,9 +31,17 @@ function Home() {
             <MyCarousel />
             <div className="welcome-title">
                 <div className="title">
-                    Welcome To
-                    <br /> KSS ENGINEERING <br />
-                    <small>our goal is always 100% customer satisfaction</small>
+                    <Zoom left cascade delay={0} duration={1000}>
+                        Welcome To
+                    </Zoom>
+                    <Zoom left cascade delay={0} duration={1000}>
+                        KSS ENGINEERING
+                    </Zoom>
+                    <small>
+                        <Zoom left cascade delay={0} duration={1000}>
+                            our goal is always 100% customer satisfaction
+                        </Zoom>
+                    </small>
                 </div>
             </div>
             <div className="container">
--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 export default function ServiceCard(props) {
     return (
-        <div className="card border-light shadow">
-            <div className="card-body">
-                <h5 className="card-subtitle mb-2 text-muted">{props.title}</h5>
-                <p className="card-text">
-                    About the kind of service we provide
-                </p>
-            </div>
-        </div>
+        <>
+            <Fade left delay={200} duration={1500}>
+                <div className="card border-light shadow">
+                    <div className="card-body">
+                        <h5 className="card-subtitle mb-2 text-muted">
+                            {props.title}
+                        </h5>
+                        <p className="card-text">
+                            About the kind of service we provide
+                        </p>
+                    </div>
+                </div>
+            </Fade>
+        </>
     );
 }
